abfb4520f0606904751ad7ee11f369d8ab7b31d9
Seyyed Ali Mohseni <mohseni676@gmail.com>
2023-06-24 23:42:05 +0300

    split InvoiceSchema

--- src/accounting/schema/invoice.schema.ts
+++ src/accounting/schema/invoice.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory, raw } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { Customer } from "./customer.schema";
+import { InvoiceItem } from "./invoice-item.schema";
 
 export type InvoiceDocument = HydratedDocument<Invoice>;
 
@@ -8,9 +10,16 @@ export class Invoice{
     @Prop({type: Date})
     invoice_date: Date
     @Prop()
+    /*     
     customer_name: string
     @Prop()
-    customer_phone: string
+    customer_phone: string 
+    */
+    // 👆 this transform to this 👇 (ManyToOne relation)
+    @Prop({type: mongoose.Types.ObjectId, ref: 'Customer'})
+    obj_customer: Customer
+
+    /*
     @Prop(raw([
         {
             good_name: String,
@@ -19,6 +28,12 @@ export class Invoice{
         }
     ]))
     obj_items: Record<string,any>[]
+    */
+    // 👆 this transform to this 👇 (OneToMany relation)
+    @Prop({type:[
+        {type: mongoose.Types.ObjectId, ref: 'InvoiceItem'}
+    ]})
+    obj_items: [InvoiceItem]
 }
 
 export const InvoiceSchema = SchemaFactory.createForClass(Invoice);
